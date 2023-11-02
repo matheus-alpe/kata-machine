fe185dd8fa010b8362a94b6faa87a586e2ba4a65
Matt <matttalves@gmail.com>
2023-11-02 17:15:13 -0300

    feat: two crystal balls algorithm

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,30 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// Given two crystal balls that will break if dropped from high enough
+// distance, determine the exact spot in which it will break in the most
+// optimized way.
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jumpAmount;
+
+    // walk square root of N and find where it breaks
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // we jump back a square root of N
+    i -= jumpAmount;
+
+    // then we linearly walk forward at most, a square root of N until we find a break
+    for (let j = 0; j < jumpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            // index which it breaks
+            return i;
+        }
+    }
+
+    // or we return -1
+    return -1;
 }
