550525f09743c448df0f9aca79352a053999c885
Matt <matttalves@gmail.com>
2023-11-15 14:49:41 -0300

    feat: queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
-    deque(): T | undefined {
+        if (!this.tail) {
+            // if doesn't have a tail there is no items on the list
+            // that's why we set tail and head
+            this.tail = this.head = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    dequeue(): T | undefined {
+        if (!this.head) return undefined;
+
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free
+        head.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
