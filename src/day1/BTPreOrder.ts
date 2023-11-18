da3bf2c4ec6ec550ea6833aa2d68c16939611136
Matt <matttalves@gmail.com>
2023-11-18 13:55:13 -0300

    feat: BinaryTree Pre Order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,21 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case:
+    if (!curr) {
+        return path;
+    }
 
+    // recurse steps:
+    // 1 pre
+    path.push(curr.value);
+
+    // 2 recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // 3 post
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
