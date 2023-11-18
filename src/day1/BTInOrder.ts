5aa84495d49719f0d0ac7f5e3f555336a0311030
Matt <matttalves@gmail.com>
2023-11-18 14:02:28 -0300

    feat: BinaryTree In Order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,21 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case:
+    if (!curr) {
+        return path;
+    }
 
+    // recurse steps:
+    // 1 pre 
+
+    // 2 recurse
+    walk(curr.left, path);
+    path.push(curr.value);
+    walk(curr.right, path);
+
+    // 3 post
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
