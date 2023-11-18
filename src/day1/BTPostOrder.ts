f6f4eafdb15948aa1a59c1b116ff0a7495a74e77
Matt <matttalves@gmail.com>
2023-11-18 14:02:41 -0300

    feat: BinaryTree Post Order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,22 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case:
+    if (!curr) {
+        return path;
+    }
 
+    // recurse steps:
+    // 1 pre
+
+    // 2 recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // 3 post
+    path.push(curr.value);
+
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
