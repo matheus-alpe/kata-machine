77ebc957041e1146e4a7dcb611515cd85c7b443c
Matt <matttalves@gmail.com>
2023-11-18 14:45:09 -0300

    feat: BinaryTree Breadth First Search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,23 @@
+// breadth first search
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: Array<BinaryNode<number> | null> = [head];
 
+    while (queue.length) {
+        // get first element of queue
+        const current = queue.shift() as BinaryNode<number> | undefined | null;
+        if (!current) {
+            continue;
+        }
+
+        // search
+        if (current.value === needle) {
+            return true;
+        }
+
+        // enqueue left/right
+        queue.push(current.left)
+        queue.push(current.right)
+    }
+
+    return false;
 }
