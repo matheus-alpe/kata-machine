5f171a108852c8ad327176fc6fe6c3534b5f752e
Matt <matttalves@gmail.com>
2023-11-21 19:47:55 -0300

    feat: breadth-first search on an adjacency matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,37 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
 
+    seen[source] = true;
+    const queue: number[] = [source]; 
+
+    do {
+        const current = queue.shift() as number;
+        if (current === needle) break;
+
+        const adjacencies = graph[current];
+        for (let i = 0; i < adjacencies.length; ++i) {
+            if (adjacencies[i] === 0 || seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = current;
+            queue.push(i);
+        }
+    } while (queue.length);
+
+    if (prev[needle] === -1) {
+        return null;
+    }
+
+    let current = needle;
+    const out: number[] = [];
+
+    while (prev[current] !== -1) {
+        out.push(current);
+        current = prev[current];
+    }
+
+    return [source].concat(out.reverse());
 }
