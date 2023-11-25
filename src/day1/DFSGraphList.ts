914cd5a58dcea376b080d3f9add5860064aa2180
Matt <matttalves@gmail.com>
2023-11-25 11:23:49 -0300

    feat: DFSGraphList

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,41 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, current: number, needle: number, seen: boolean[], path: number[]): boolean {
+    if (seen[current]) return false;
 
+    seen[current] = true;
+
+    // recurse steps:
+    // pre
+    path.push(current);
+    if (current === needle) {
+        return true;
+    }
+
+    // recurse
+    const list = graph[current];
+    for (let i = 0; i < list.length; ++i) {
+        const edge = list[i];
+
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+
+    // posts
+    path.pop();
+
+    return false;
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
+
+    walk(graph, source, needle, seen, path)
+    
+    if (path.length === 0) {
+        return null;
+    }
+
+    return path;
 }
